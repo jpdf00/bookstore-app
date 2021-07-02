0cf08f1650b15a86b2bd1c48ad1bc6c50241e678
jpdf00 <j.pdf00@gmail.com>
2021-07-02 19:15:24 -0300

    Add uuidV4 to generate random Id's
    - Optional sugestion by TSE

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers/index';
 
 const BOOKS = [
   {
-    id: Math.floor(Math.random() * 10000),
+    id: uuidv4(),
     title: 'Book Test 1',
     category: 'Action',
   },
   {
-    id: Math.floor(Math.random() * 10000),
+    id: uuidv4(),
     title: 'Book Test 2',
     category: 'Horror',
   },
   {
-    id: Math.floor(Math.random() * 10000),
+    id: uuidv4(),
     title: 'Book Test 3',
     category: 'Sci-Fi',
   },
